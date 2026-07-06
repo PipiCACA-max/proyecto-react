10bb58c9635519867f01c0aa008ad00a8ab9f7ed
Alessandro <dbzcepo@gmail.com>
2026-07-06 17:52:29 -0600

    google-site-verification 06/07/26

--- src/Pages/Appointment.js
+++ src/Pages/Appointment.js
@@ -1,8 +1,8 @@
-import React from "react";
-import AppointmentForm from "../Components/AppointmentForm";
+import React from 'react'
+import AppointmentForm from '../Components/AppointmentForm'
 
 function Appointment() {
-  return <AppointmentForm />;
+  return <AppointmentForm />
 }
 
-export default Appointment;
+export default Appointment
